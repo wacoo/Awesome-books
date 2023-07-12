6986a1f7410798eb7bb59952d65946960fec416a
wac <wabaham9@gmail.com>
2023-07-12 08:57:22 -0500

    Add navbar and tabs

---
+++ static/js/nav.js
@@ -0,0 +1,41 @@
+const add = document.querySelector('.wrapper_frm');
+const list = document.querySelector('.wrapper_list');
+const contact = document.querySelector('.wrapper_contact');
+
+const btnList = document.getElementById('btn_list');
+const btnAdd = document.getElementById('btn_add');
+const btnContact = document.getElementById('btn_contact');
+
+btnList.addEventListener('click', () => {
+  add.style.display = 'none';
+  list.style.display = 'block';
+  contact.style.display = 'none';
+  btnAdd.classList.remove('active');
+  btnList.classList.add('active');
+  btnContact.classList.remove('active');
+});
+btnAdd.addEventListener('click', () => {
+  add.style.display = 'flex';
+  list.style.display = 'none';
+  contact.style.display = 'none';
+  btnAdd.classList.add('active');
+  btnList.classList.remove('active');
+  btnContact.classList.remove('active');
+});
+btnContact.addEventListener('click', () => {
+  add.style.display = 'none';
+  list.style.display = 'none';
+  contact.style.display = 'flex';
+  btnAdd.classList.remove('active');
+  btnList.classList.remove('active');
+  btnContact.classList.add('active');
+});
+
+function showDefaultTab() {
+  add.style.display = 'none';
+  list.style.display = 'block';
+  contact.style.display = 'none';
+  btnList.className = 'active';
+}
+
+showDefaultTab();
